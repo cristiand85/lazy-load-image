9eb2ecc7ef5553ee48521bf9bdf07f4581395abc
Cristian <dolcecristian@gmail.com>
2014-07-07 11:49:38 +0200

    Create lazy-ie7.js

---
+++ lazy-ie7.js
@@ -0,0 +1,20 @@
+document.querySelectorAll||(document.querySelectorAll=function(a){var b=document,c=b.documentElement.firstChild,d=b.createElement("STYLE");return c.appendChild(d),b.__qsaels=[],d.styleSheet.cssText=a+"{x:expression(document.__qsaels.push(this))}",window.scrollBy(0,0),b.__qsaels});
+(function(){
+    var lazy = Array.prototype.concat.apply([],document.querySelectorAll('*[data-lazy-src]'));
+    window.onscroll = function(e){
+       loadImage();
+    }
+    function loadImage(){
+        for(var i in lazy){
+          var image = lazy[i],
+          scrollTop = window.pageXOffset || document.body.scrollTop || document.documentElement.scrollTop,
+          imageTop=image.getBoundingClientRect()['top'];
+           if(imageTop < scrollTop){
+             image.src = image.getAttribute('data-lazy-src');
+              image.removeAttribute('data-lazy-src');
+              delete lazy[i];
+           }
+        }
+    }
+    loadImage();
+})();
