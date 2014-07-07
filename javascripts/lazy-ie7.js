cb5b73e893903fc68184c4e26e44d2bb7f0d623c
Cristian <dolcecristian@gmail.com>
2014-07-07 12:39:54 +0200

    Create lazy-ie7.js

---
+++ javascripts/lazy-ie7.js
@@ -0,0 +1,27 @@
+/* lazy.js (c) Cristian Dolce
+ * semplice e piccolo script per il caricamento asincrono delle immagini
+ * MIT License
+ *
+ * esempio:  
+ * `<img src="1.png" data-lazy-src="image.jpg">`
+ */
+document.querySelectorAll||(document.querySelectorAll=function(a){var b=document,c=b.documentElement.firstChild,d=b.createElement("STYLE");return c.appendChild(d),b.__qsaels=[],d.styleSheet.cssText=a+"{x:expression(document.__qsaels.push(this))}",window.scrollBy(0,0),b.__qsaels});
+(function(){
+    var lazy = Array.prototype.concat.apply([],document.querySelectorAll('*[data-lazy-src]'));
+    window.onscroll = function(e){
+       loadImage();
+    }
+    function loadImage(){
+        for(var i in lazy){
+          var image = lazy[i],
+          scrollTop = window.pageXOffset || document.body.scrollTop || document.documentElement.scrollTop,
+          imageTop=image.getBoundingClientRect()['top'];
+           if(imageTop < scrollTop){
+             image.src = image.getAttribute('data-lazy-src');
+              image.removeAttribute('data-lazy-src');
+              delete lazy[i];
+           }
+        }
+    }
+    loadImage();
+})();
